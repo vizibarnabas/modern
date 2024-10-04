f1143656715ba97e355dfee7b754cb8df8dfb521
vizibarnabas <56293662+vizibarnabas@users.noreply.github.com>
2024-10-04 16:10:00 +0200

    pyton

---
+++ myfunc.py
@@ -0,0 +1,2 @@
+def myfuct(param1: int, param2: int)->int:
+    return param1+param2
---
+++ test_myfunct.py
@@ -0,0 +1,6 @@
+import unittest
+import myfunc
+class TestMyFunc(unitest.TestCase):
+
+    def test_my_func(self):
+        self.asertEqual(myfunc(1,1)2)
